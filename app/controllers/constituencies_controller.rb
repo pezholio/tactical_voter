7d7c0f6b866103738ca446c9be7b737366dbe624
Stuart Harrison <pezholio@gmail.com>
2019-11-26 08:12:59 +0000

    Show error if constituency doesn't exist

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -4,5 +4,8 @@ class ConstituenciesController < ApplicationController
 
   def search
     @constituency = ConstituencySearch.new(postcode: params[:postcode]).find
+    unless @constituency.present?
+      redirect_to "/", alert: "Postcode doesn't exist"
+    end
   end
 end
